e40f592028ee4eda98bfe58eb28901ab40e0576d
西片 <32992109+allinu@users.noreply.github.com>
2022-09-15 11:49:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+
 print('请输入sudo密码')
 #os.system("rm -rf ~/Library/Application\ Support/com.nssurge.surge-*")
 os.system("sudo -S route add 17.253.84.123/32 1.2.3.4")
@@ -10,6 +11,6 @@ os.system("sudo -S route add 17.253.114.125/32 1.2.3.4")
 os.system("sudo -S date 010110002021")
 os.system("""osascript -e 'tell app "Surge" to open'""")
 time.sleep(20)
-os.system("sudo -S sntp -sS cn.ntp.org.cn")
+os.system("sudo -S sntp -sS time.apple.com.")
 os.system("netstat -rn |grep 1.2.3.4")
 time.sleep(3)
